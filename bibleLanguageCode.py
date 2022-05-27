4b808072ff981d36f30d737358138b380210ceab
giovanni <144000giovanni@gmail.com>
2022-05-27 12:23:08 +1200

    'update'

--- bibleLanguageCode.py
+++ bibleLanguageCode.py
@@ -1,15 +1,15 @@
 import json
 
+while True:
+ fopen = open("bibleLanguageCode.json", "r")
+ jsondata = json.loads(fopen.read())
+ fopen.close()
+ print(jsondata)
 
-fopen = open("bibleLanguageCode.json", "r")
-jsondata = json.loads(fopen.read())
-fopen.close()
-print(jsondata)
 
+ bibleversion = input("bibleversion: " )
+ langInput = input("languageCode: " )
+ jsondata["VERSIONLANGUAGE"][bibleversion] = langInput
 
-bibleversion = input("bibleversion: " )
-langInput = input("languageCode: " )
-jsondata["VERSIONLANGUAGE"][bibleversion] = langInput
-
-with open("bibleLanguageCode.json", "w") as outfile:
+ with open("bibleLanguageCode.json", "w") as outfile:
   json.dump(jsondata, outfile, indent=4, sort_keys=True)
