7af259995de2b3ad3095b2c9a878d6668b55ee2c
giovanni <144000giovanni@gmail.com>
2022-11-02 04:49:50 +1300

    'update'

--- UZB/UZB/checkForMissingVerses.py
+++ UZB/UZB/checkForMissingVerses.py
@@ -2,6 +2,8 @@ import json
 
 f = open("UZB.json", "r", encoding="utf8")
 jsondata = json.loads(f.read())["version"]
+count = 1
+string = ""
 for key in jsondata:
 
   book_name = jsondata[key]["book_name"]
@@ -14,5 +16,13 @@ for key in jsondata:
       verse = verses[v]["verse"]
       if verse=="":
         reference = book_name + " " + c + ":"+v
-        print(reference)
+        print(str(count)+ ". " + reference)
         print(verse)
+        string = string + str(count)+ ". " + reference
+        string = string + verse
+        string = string + "\n\n"
+        count = count + 1
+
+print(string)        
+with open("../notexist.txt", "w") as writer:
+  writer.write(string)        
